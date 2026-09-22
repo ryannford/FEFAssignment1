5b892ccd6c7f7af7ecc8a54609c42ec918d270ac
Ryan Ford <ryanford1025@gmail.com>
2026-09-22 17:56:33 -0300

    Finished the validateSSN method, not using regex

--- Employee.ts
+++ Employee.ts
@@ -3,30 +3,76 @@
 // DATE:    22/09/2026
 
 export abstract class Employee {
-  public ssn: string = "";
-  public lastName: string = "";
-  public firstName: string = "";
-  public address: string = "";
-  public rank: number = 0;
-  public age: number = 0;
+  public ssn: string;
+  public lastName: string;
+  public firstName: string;
+  public address: string;
+  public rank: number;
+  public age: number;
 
+  constructor(
+    ssn: string,
+    lastName: string,
+    firstName: string,
+    address: string,
+    rank: number,
+    age: number,
+  ) {
+    this.ssn = ssn;
+    this.lastName = lastName;
+    this.firstName = firstName;
+    this.address = address;
+    this.rank = rank;
+    this.age = age;
+  }
+
+  /**
+   * Checks the age value to ensure it is 16 or older
+   * @returns { Boolean } - Returns true if they are 16 or older
+   */
   protected validateAge(): boolean {
     if (this.age < 16) {
-      throw new Error("**The employee must be 16 or older**");
+      throw new Error("** INVALID AGE: The employee must be 16 or older **");
     }
 
     return true;
   }
 
+  /**
+   * Checks the rank value to ensure the rank number is between 1-5 (inclusive)
+   * @returns { Boolean } - Returns true if rank number is between 1-5 (inclusive)
+   */
   protected validateRank(): boolean {
     if (this.rank < 1 || this.rank > 5) {
-      throw new Error("**An employee's rank must be between 1-5**");
+      throw new Error(
+        "** INVALID RANK: An employee's rank must be between 1-5 **",
+      );
     }
 
     return true;
   }
 
+  /**
+   * Checks SSN to ensure it is formatted properly (###-###-###)
+   * @returns { Boolean } - Returns true if SSN is formatted properly (###-###-###)
+   */
   protected validateSSN(): boolean {
-    if(this.ssn !== )
+    let ssn = this.ssn;
+
+    if (ssn.length !== 11 || ssn[3] !== "-" || ssn[7] !== "-") {
+      throw new Error(
+        "** INVALID SSN: SSN must be formatted as ###-###-### **",
+      );
+    }
+
+    const ssnParts = ssn.split("-");
+
+    for (let part of ssnParts) {
+      if (isNaN(parseInt(part))) {
+        throw new Error("** INVALID SSN: SSN must be whole numbers **");
+      }
+    }
+
+    return true;
   }
 }
